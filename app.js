8c9ef8d9fb86c994f0418b76a873541505137e24
AnkitaBagale <manali.bagale@gmail.com>
2020-10-24 21:39:58 +0530

    about page added

--- app.js
+++ app.js
@@ -44,27 +44,6 @@ function displaySlide(n){
     }
 
     slides[slideNo-1].style.display = "block";
-    dots[slideNo-1].className += " active";
-    console.log(slideNo); 
+    dots[slideNo-1].className += " active"; 
 }
 
-const navSlide = () =>{
-    const burger = document.querySelector('.Burger');
-    const nav=document.querySelector('.Nav-Links');
-    const navLinks=document.querySelectorAll('.Nav-Links li')
-    burger.addEventListener('click', ()=>{
-        //toggle menu
-        nav.classList.toggle('Nav-Active');
-        navLinks.forEach((link, index)=>{
-            if (link.style.animation){
-                link.style.animation='';
-            }
-            else{
-            link.style.animation=`navLinksFade 0.5s ease forwards ${index*100+400}ms`;
-            }
-        })
-        burger.classList.toggle('toggle');
-    })
-
-}
-navSlide();
